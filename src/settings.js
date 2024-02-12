fc48aa2901d9e7048d581a7c303243f6c5c649c6
Stefanos Tzortzoglou <34420752+StefTzor@users.noreply.github.com>
2024-02-12 17:03:51 +0100

    firebaseconfig test

--- src/settings.js
+++ src/settings.js
@@ -8,13 +8,13 @@
 -------------------------------- */
 const firebaseConfig = {
   apiKey: "AIzaSyDqh0NM-ZRNIW-Csl-YQS3J6BOAjkUt68o",
-  databaseURL: "https://kcc-be-my-valentine-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "kcc-be-my-valentine",
-  storageBucket: "kcc-be-my-valentine.appspot.com",
-  messagingSenderId: "461637234904",
-  appId: "1:461637234904:web:0cb468809b249c602867ad",
-  measurementId: "G-QD987DDLLQ"
-  authDomain: "kcc-be-my-valentine.firebaseapp.com",
+    authDomain: "kcc-be-my-valentine.firebaseapp.com",
+    databaseURL: "https://kcc-be-my-valentine-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "kcc-be-my-valentine",
+    storageBucket: "kcc-be-my-valentine.appspot.com",
+    messagingSenderId: "461637234904",
+    appId: "1:461637234904:web:0cb468809b249c602867ad",
+    measurementId: "G-QD987DDLLQ"
 };
 
 /* CLOUD FIRESTORE CONFIG
